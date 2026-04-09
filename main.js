4f3084b75c19901d3affd5bf36d0208e94a3135c
H3110 <96912707+Desenvolvimento15@users.noreply.github.com>
2026-04-09 13:38:41 -0300

    vs01

---
+++ main.js
@@ -0,0 +1,19 @@
+$(".slick-one").slick({
+  lazyLoad: "ondemand",
+  arrows: false,
+  dots: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+});
+
+$(".slick-five").slick({
+  lazyLoad: "ondemand",
+  arrows: false,
+  dots: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5000,
+});
